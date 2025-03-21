27f71017fd18d844e7ff0bfb60d48ab8774a2a7e
Dylan Collins <pyxyllv@gmail.com>
2025-03-21 10:04:32 +0000

    add filters to only get ONE video with the video source being youtube. added iframe for video playback

--- movie-app/src/components/movieVideos/index.jsx
+++ movie-app/src/components/movieVideos/index.jsx
@@ -1,17 +1,10 @@
-import React, { useEffect, useState } from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
+import React from "react";
+import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
-import { Link } from "react-router";
-import { getMovieVideos } from "../../api/tmdb-api";
-import { excerpt } from "../../util";
 import { useQuery } from "@tanstack/react-query";
-import Spinner from '../spinner'
-
+import { getMovieVideos } from "../../api/tmdb-api";
+import Spinner from '../spinner';
+import Box from "@mui/material/Box";
 
 export default function MovieVideos({ movie }) {
   const { data, error, isPending, isError } = useQuery({
@@ -26,11 +19,52 @@ export default function MovieVideos({ movie }) {
   if (isError) {
     return <h1>{error.message}</h1>;
   }
-  
-  const videos = data.results[0];
 
+  if (!data.results || data.results.length === 0) {
+    return (
+      <Paper sx={{ padding: 2, marginTop: 2 }}>
+        <Typography variant="h6">No videos available for this movie</Typography>
+      </Paper>
+    );
+  }
+  
+  // Filter for YouTube trailers only
+  const youtubeTrailer = data.results.find(
+    video => video.site.toLowerCase() === "youtube" && 
+    video.type.toLowerCase() === "trailer"
+  );
+  
+  // If no YouTube trailer is found
+  if (!youtubeTrailer) {
+    return (
+      <Paper sx={{ padding: 2, marginTop: 2 }}>
+        <Typography variant="h6">No trailer available for this movie</Typography>
+      </Paper>
+    );
+  }
 
   return (
-    <a href={`https://youtube.com/watch?v=${videos.key}`}>video</a>
+    <Paper sx={{ padding: 2, marginTop: 2 }}>
+      <Typography variant="h5" gutterBottom>
+        Official Trailer
+      </Typography>
+      
+      <Box sx={{ width: '100%', height: '400px', marginBottom: 2 }}>
+        <iframe
+          width="100%"
+          height="100%"
+          src={`https://www.youtube.com/embed/${youtubeTrailer.key}`}
+          title={youtubeTrailer.name}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </Box>
+      
+      <Box sx={{ mt: 2 }}>
+        <Typography variant="body2" color="text.secondary">
+          {youtubeTrailer.name}
+        </Typography>
+      </Box>
+    </Paper>
   );
 }
