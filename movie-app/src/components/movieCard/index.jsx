b7bea79cf5cb2b4e6334418b574f10a97de56954
Dylan Collins <pyxyllv@gmail.com>
2025-03-23 21:45:18 +0000

    Started Layout Modifications for movie card display

--- movie-app/src/components/movieCard/index.jsx
+++ movie-app/src/components/movieCard/index.jsx
@@ -10,7 +10,6 @@ import Typography from "@mui/material/Typography";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import CalendarIcon from "@mui/icons-material/CalendarTodayTwoTone";
 import StarRateIcon from "@mui/icons-material/StarRate";
-import IconButton from "@mui/material/IconButton";
 import Grid from "@mui/material/Grid2";
 import img from "../../images/film-poster-placeholder.png";
 import Avatar from "@mui/material/Avatar";
@@ -121,7 +120,7 @@ export default function MovieCard({ movie, action }) {
         }}
       >
         {action(movie)}
-        <Link to={`/movies/${movie.id}`} style={{ textDecoration: "none" }}>
+        <Link to={`/movies/${movie.id}`}>
           <Button
             variant="contained"
             size="medium"
